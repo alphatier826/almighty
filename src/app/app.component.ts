4fb3425db0efdd5b929e70518bb6d937868fcb82
rsubbaiya <nkumar@apptium.com>
2023-02-18 00:10:24 +0530

    Phase 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig, NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgIf } from '@angular/common';
+import { Observable } from 'rxjs';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [NgbCarouselConfig]
 })
 export class AppComponent implements OnInit {
   title = 'AHMv2';
@@ -20,5 +23,11 @@ export class AppComponent implements OnInit {
       this.newsContents.push(index+1 +' - Do Media Vultures Perpetuate Mass Shootings?');
       
     }
+    this.listResources
+  }
+
+  listResources(): Observable<any> {
+    const obj:any = {name:'nik'};
+    return obj;
   }
 }
--- src/app/components/ahm-pricing/ahm-pricing.component.ts
+++ src/app/components/ahm-pricing/ahm-pricing.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AhmPricingComponent implements OnInit {
 
+  prices = []
+
   constructor() { }
 
   ngOnInit() {
+    const price = [{
+      title: 'MONTHLY',
+      amount: '11,500',
+      features: ['Completely Automated', 'Trades Indices', 'WhatsApp Support', 'Remote Support', '1 Strategy', '1000 Rs.Maintenance Charges For Every Month'],
+      isActive: false
+    },{
+      title: 'QUARTERLY',
+      amount: '28,000',
+      features: ['Completely Automated', 'Trades Indices', 'WhatsApp Support', 'Remote Support', '3-4 Strategy', '1000 Rs.Maintenance Charges For Every Month'],
+      isActive: false
+    },{
+      title: 'HALF YEARLY',
+      amount: '48,000',
+      features: ['Completely Automated', 'Trades Indices', 'WhatsApp Support', 'Remote Support', '10 - 15 Strategy', '1000 Rs.Maintenance Charges For Every Month'],
+      isActive: false
+    },{
+      title: 'YEARLY',
+      amount: '88,000',
+      features: ['Completely Automated', 'Trades Indices', 'WhatsApp Support', 'Remote Support', '20 - 25 Strategy', '1000 Rs.Maintenance Charges For Every Month'],
+      isActive: false
+    }];
+    this.prices = price;
   }
 
 }
