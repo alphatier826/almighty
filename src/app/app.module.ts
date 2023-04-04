a75a45b07c19a0dfa786f56b0bee0080fc84519f
rsubbaiya <nkumar@apptium.com>
2023-04-04 13:00:13 +0530

    Form Module Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -19,7 +21,8 @@ import { AhmAdminModule } from './components/ahm-admin/ahm-admin.module';
     AppRoutingModule,
     AhmAdminModule,
     AhmHomeModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
